c23ca59b9696279a92f8cdd8699210980aeae165
jedsalanguit <jlyndon.salanguit@gmail.com>
2025-05-19 23:19:12 +0800

    Add Firebase

---
+++ firebase/client.ts
@@ -0,0 +1,18 @@
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
+import { getApp, getApps, initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0IWryF4eiXKL15JgMUOqV_YDuwXCM_Ms",
+  authDomain: "nexivue.firebaseapp.com",
+  projectId: "nexivue",
+  storageBucket: "nexivue.firebasestorage.app",
+  messagingSenderId: "119501007299",
+  appId: "1:119501007299:web:efc0cded92ef2f87f77576",
+  measurementId: "G-GPVM6Z7244",
+};
+
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
