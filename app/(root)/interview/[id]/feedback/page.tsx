4a80fd053c27251a5d376e5eea7ed4294ae2f3cc
jedsalanguit <jlyndon.salanguit@gmail.com>
2025-06-19 09:42:16 +0800

    Create Interview feedback

---
+++ app/(root)/interview/[id]/feedback/page.tsx
@@ -0,0 +1,24 @@
+import { getCurrentUser } from "@/lib/actions/auth.action";
+import {
+  getFeedbackByInterviewId,
+  getInterviewById,
+} from "@/lib/actions/general.action";
+import { redirect } from "next/navigation";
+
+const page = async ({ params }: RouteParams) => {
+  const { id } = await params;
+  const user = await getCurrentUser();
+
+  const interview = await getInterviewById(id);
+  if (!interview) redirect("/");
+
+  const feedback = await getFeedbackByInterviewId({
+    interviewId: id,
+    userId: user?.id!,
+  });
+
+  console.log(feedback);
+  return <div>page</div>;
+};
+
+export default page;
--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { generator, interviewer } from "@/constants";
+import { createFeedback } from "@/lib/actions/general.action";
 import { cn } from "@/lib/utils";
 import { vapi } from "@/lib/vapi.sdk";
 import Image from "next/image";
@@ -70,10 +71,11 @@ const Agent = ({
     console.log("Generate feedback here.");
 
     //TODO: Create a server action that generates feedback
-    const { success, id } = {
-      success: true,
-      id: "feedback-id",
-    };
+    const { success, feedbackId: id } = await createFeedback({
+      interviewId: interviewId!,
+      userId: userId!,
+      transcript: messages,
+    });
 
     if (success && id) {
       router.push(`/interview/${interviewId}/feedback`);
